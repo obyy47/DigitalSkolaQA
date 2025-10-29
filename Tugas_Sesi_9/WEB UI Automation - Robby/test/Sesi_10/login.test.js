c10cd80786fa51667faaf3e2502943238d7eb9d3
Robby Adriansyah Fadillah <robbyadriansyah7@gmail.com>
2025-10-29 16:37:07 +0700

    Sesi 10 - Web UI Automation Advance Part 1

--- Tugas_Sesi_9/WEB UI Automation - Robby/test/Sesi_10/login.test.js
+++ Tugas_Sesi_9/WEB UI Automation - Robby/test/Sesi_10/login.test.js
@@ -12,7 +12,7 @@ describe('Tugas Sesi 10', function () {
             driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build()
             await login(driver);
         })
-        afterEach(() => driver.quit());
+        afterEach(() => driver.quit())
 
         it('sukses login ke web SauceDemo di browser Chrome', async () => {
             assert.ok((await driver.getCurrentUrl()).includes('inventory'))
@@ -22,8 +22,8 @@ describe('Tugas Sesi 10', function () {
     describe('Firefox', () => {
         beforeEach(async () => {
             driver = await new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().addArguments('-headless')).build()
-            await login(driver);
-        });
+            await login(driver)
+        })
         afterEach(() => driver.quit())
 
         it('sukses login ke web SauceDemo di browser Firefox', async () => {
